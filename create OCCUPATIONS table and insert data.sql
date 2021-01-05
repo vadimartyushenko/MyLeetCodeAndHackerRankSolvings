df5ab3d5fa1310efde48c61a2ddde9180ded2d72
Artyushenko Vadim <artushenkomail@mail.ru>
2021-01-05 14:35:00 +0700

    add solving problem)

--- create OCCUPATIONS table and insert data.sql
+++ create OCCUPATIONS table and insert data.sql
@@ -23,3 +23,9 @@ values
 --('Maria', 'Actor');
 --('Meera', 'Singer');
   ('Ashely', 'Professor');
+--решение
+select name ||'('|| substr(occupation,1,1)||')' from OCCUPATIONS
+order by name;
+select 'There are a total of ' || count (occupation) ||' ' || lower(occupation)||'s.' from OCCUPATIONS
+group by occupation
+order by count (occupation), lower(occupation);
