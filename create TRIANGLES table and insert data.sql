e23cc31f5eb6da519f0ddac4f038b0e6ad3bfad6
Artyushenko Vadim <artushenkomail@mail.ru>
2021-01-05 13:30:17 +0700

    Create create TRIANGLES table and insert data.sql

---
+++ create TRIANGLES table and insert data.sql
@@ -0,0 +1,26 @@
+-- Create table
+create table TRIANGLES
+(
+  a     integer not null,
+  b   integer not null,
+  c  integer not null
+)
+tablespace USERS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+insert into TRIANGLES s
+(s.a, s.b, s.c)
+values
+--(20, 20, 23);
+--(20, 20, 20);
+--(20, 21, 22);  
+(13, 14, 30);  
+  
