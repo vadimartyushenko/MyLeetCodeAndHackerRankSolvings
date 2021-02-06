0555ba00ff3ba2ec83dcf394fb9f440926bf8a98
Artyushenko Vadim <artushenkomail@mail.ru>
2021-02-06 15:46:29 +0700

    Update create BST and insert data.sql

--- Binary Tree Nodes/create BST and insert data.sql
+++ Binary Tree Nodes/create BST and insert data.sql
@@ -3,3 +3,10 @@ create table bst
   n integer not null,
   p integer
 );
+insert into bst (n, p) values (1, 2);
+insert into bst (n, p) values (3, 2);
+insert into bst (n, p) values (6, 8);
+insert into bst (n, p) values (9, 8);
+insert into bst (n, p) values (2, 5);
+insert into bst (n, p) values (8, 5);
+insert into bst (n, p) values (5, null);
