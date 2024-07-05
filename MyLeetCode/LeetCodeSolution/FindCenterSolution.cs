a0557959705240bc6930756d231c3acdacf86c90
vadimartyushenko <vadim.artyuschenko@yandex.ru>
2024-07-05 13:04:23 +0700

    added FindCenter solution

---
+++ MyLeetCode/LeetCodeSolution/FindCenterSolution.cs
@@ -0,0 +1,16 @@
+namespace LeetCodeSolution;
+
+public class FindCenterSolution
+{
+    public static void Solve()
+    {
+        int[][] edges = [[1, 2], [2, 3], [4, 2]];
+        var result = FindCenter(edges);
+    }
+    
+    static int FindCenter(int[][] edges)
+    {
+        return edges[0][0] == edges[1][0] ? edges[0][0] : edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+    
+}
--- MyLeetCode/LeetCodeSolution/Program.cs
+++ MyLeetCode/LeetCodeSolution/Program.cs
@@ -1,11 +1,14 @@
 ﻿using LeetCodeSolution;
 
-var task_name = "AddTwoNumbers";
+var task_name = "FindCenter";
 
 switch (task_name)
 {
     case "AddTwoNumbers":
         AddTwoNumbersSolution.Solve();
+        break;
+    case "FindCenter":
+        FindCenterSolution.Solve();
         break;
     default:
         throw new ArgumentException("Unexpected task");
